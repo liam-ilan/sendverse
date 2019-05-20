24f4e5a10a6bb11deb54127227430522e1d2c87b
Liam Ilan <snowboardsheep@gmail.com>
2019-05-20 09:24:51 -0700

    You can now enter a room by pressing enter.

--- public/homepage/js/index.js
+++ public/homepage/js/index.js
@@ -4,3 +4,10 @@ const enter = document.getElementById('enter');
 enter.addEventListener('click', () => {
   window.location.href += encodeURIComponent(roomName.value);
 });
+
+document.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    window.location.href += encodeURIComponent(roomName.value);
+  }
+});
