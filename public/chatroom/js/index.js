f78f9c5f9dd69c4057cb85656f2f101652a25ce8
Liam Ilan <snowboardsheep@gmail.com>
2019-06-24 09:53:48 -0700

    Simplified and improved code for index.js in the chatroom.

--- public/chatroom/js/index.js
+++ public/chatroom/js/index.js
@@ -1,20 +1,52 @@
 /* global io v */
-
-const IOS = !!navigator.platform.match(/iPhone|iPod|iPad/);
-// connect the socket
 const socket = io.connect(window.location.href.toLowerCase());
 
-// get elements
 const share = document.getElementById('share');
 const input = document.getElementById('input');
 const sendButton = document.getElementById('send-button');
 const history = document.getElementById('history');
 
-// our color
+// color of your messages. Each participant has their color chosen at random.
 const ourColor = `#${Math.floor(Math.random() * 4095).toString(16).padStart(3, '0')}`;
 
-// prevent enter from adding a line break
-document.addEventListener('keypress', (e) => { if (e.key === 'Enter') { e.preventDefault(); } });
+// calculate the color the font should be depending on a background color
+// for a hex with a length of 4
+function fontColor(color) {
+  const r = parseInt(color.charAt(1), 16);
+  const g = parseInt(color.charAt(2), 16);
+  const b = parseInt(color.charAt(3), 16);
+  return Math.max(r, g, b) > 10 ? 'black' : 'white';
+}
+
+// add a bubble
+function addBubble(color, message, myBubble = false) {
+  const bubble = document.createElement('div');
+  bubble.className = `bubble ${myBubble ? 'my-bubble' : ''}`;
+  bubble.innerHTML = v.escapeHtml(v.unescapeHtml(message));
+  bubble.style.backgroundColor = color;
+  bubble.style.color = fontColor(color);
+
+  history.appendChild(bubble);
+
+  // scroll down to keep recent messages in view
+  history.scrollTop = history.scrollHeight;
+}
+
+// post a message
+function postMessage() {
+  const data = { message: input.innerText, color: ourColor };
+
+  if (data.message !== '') {
+    addBubble(data.color, input.innerText, true);
+
+    // emit the message
+    socket.emit('message', data);
+    input.innerText = '';
+  }
+}
+
+// when you click the sendbutton, post the message
+sendButton.addEventListener('click', postMessage);
 
 // remove styling from copy paste
 input.addEventListener('paste', (e) => {
@@ -28,109 +60,44 @@ input.addEventListener('paste', (e) => {
   document.execCommand('insertHTML', false, text);
 });
 
-// calculate the color the font should be depending on a background color
-// for a hex with a length of 4
-function fontColor(color) {
-  const r = parseInt(color.charAt(1), 16);
-  const g = parseInt(color.charAt(2), 16);
-  const b = parseInt(color.charAt(3), 16);
-  return Math.max(r, g, b) > 10 ? 'black' : 'white';
-}
-
-// add a bubble
-function addBubble(color, message, myBubble = false) {
-  // create a bubble
-  const bubble = document.createElement('div');
-  bubble.className = `bubble ${myBubble ? 'my-bubble' : ''}`;
-  bubble.innerHTML = v.escapeHtml(v.unescapeHtml(message));
-  bubble.style.backgroundColor = color;
-  bubble.style.color = fontColor(color);
-
-  // add the bubble
-  history.appendChild(bubble);
-
-  // scroll down
-  history.scrollTop = history.scrollHeight;
-}
-
-// post a message
-function postMessage() {
-  // get the data
-  const data = { message: input.innerText, color: ourColor };
-  if (data.message !== '') {
-    // add the bubble
-    addBubble(data.color, input.innerText, true);
-
-    // emit the message
-    socket.emit('message', data);
-    input.innerText = '';
+// prevent enter from adding a line break in input so that Enter sends message.
+input.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    postMessage(e);
   }
-}
-
-// on every message
-socket.on('message', (data) => {
-  addBubble(data.color, data.message);
 });
 
-// when you click the sendbutton, post the message
-sendButton.addEventListener('click', postMessage);
-
-// when you press enter, post the message
-input.addEventListener('keyup', (e) => {
-  if (e.key === 'Enter') { postMessage(e); }
-});
-
-
-// copy text to clipboard (for IOS)
-function copyToClipboard(text) {
-  // create the element
-  const element = document.createElement('textarea');
-
-  // set content editable to true
+// Copy Link functionality.
+share.addEventListener('click', () => {
+  const element = document.createElement('textarea'); // set temp element.
   element.contentEditable = 'true';
   element.readOnly = 'false';
-
-  // put our text inside
-  element.innerHTML = text;
-
-  // add element
+  element.innerHTML = window.location.href; // this is what we want copied.
   document.body.appendChild(element);
 
-  // if IOS
-  if (IOS) {
-    // get range
+  if (!!navigator.platform.match(/iPhone|iPod|iPad/)) {
     const range = document.createRange();
-
-    // select
     range.selectNodeContents(element);
 
-    // get selection
     const selection = window.getSelection();
     selection.removeAllRanges();
     selection.addRange(range);
 
     element.setSelectionRange(0, 999999);
-
-    // copy
-    document.execCommand('copy');
   } else {
-    // select and copy
     element.select();
-    document.execCommand('copy');
   }
 
-  // remove element
-  document.body.removeChild(element);
-}
+  document.execCommand('copy');
+  document.body.removeChild(element); // cleanup.
 
-// when share is clicked
-share.addEventListener('click', () => {
-  // cppy to clipboard
-  copyToClipboard(window.location.href);
-
-  // Say "Coppied"
-  share.innerHTML = 'Coppied';
-
-  // after 1 secomd, change back to "Copy Link"
+  // button UI
+  share.innerHTML = 'Copied';
   window.setTimeout(() => { share.innerHTML = 'Copy Link'; }, 1000);
+});
+
+// Listen to server events.
+socket.on('message', (data) => {
+  addBubble(data.color, data.message);
 });
